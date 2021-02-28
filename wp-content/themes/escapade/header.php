3d3b613a8abba087aa47dcabc439a61526d17d22
Minhyeok <alsgur0008@naver.com>
2021-03-01 02:00:51 +0900

    모바일 메뉴, 헤더 백그라운드 이미지 스타일 수정

--- wp-content/themes/escapade/header.php
+++ wp-content/themes/escapade/header.php
@@ -34,7 +34,7 @@
 
 	<?php do_action( 'primer_before_header' ); ?>
 
-	<header id="masthead" class="site-header" role="banner"<?php if ( primer_has_hero_image() ) : ?> style="background:url('<?php echo esc_url( primer_get_hero_image() ); ?>') no-repeat top center; background-size: contain;"<?php endif; ?>>
+	<header id="masthead" class="site-header" role="banner"<?php if ( primer_has_hero_image() ) : ?> style="background:url('<?php echo esc_url( primer_get_hero_image() ); ?>') no-repeat top center; background-size: contain;background-position: center;"<?php endif; ?>>
 
 		<?php do_action( 'primer_pre_hero' ); ?>
 
